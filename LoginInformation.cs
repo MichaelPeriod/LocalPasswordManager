9a0daa9587163d1c250fb84dc0b903afec87a0dd
MichaelPeriod <39890684+MichaelPeriod@users.noreply.github.com>
2023-12-04 12:53:24 -0500

    Got the UI to automatically add rows for passwords

---
+++ LoginInformation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class LoginInformation
+{
+    private readonly string _website;
+    private readonly string _username;
+    private readonly string _password;
+
+    public LoginInformation(string website, string username, string password)
+    {
+        _website = website;
+        _username = username;
+        _password = password;
+    }
+
+    public String GetWebsite()
+    {
+        return _website;
+    }
+
+    public String GetUsername() { return _username; }
+
+    public String GetPassword() { return _password; } //used for encryption later
+}
--- PasswordBrowser.xaml.cs
+++ PasswordBrowser.xaml.cs
@@ -19,9 +19,44 @@ namespace LocalPasswordManager
     /// </summary>
     public partial class PasswordBrowser : Window
     {
+        int currRowPos = 2;
         public PasswordBrowser()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CreateLine(new LoginInformation("abc.com", "user1", "*****"));
+          
+            currRowPos++;
+        }
+
+        private void CreateLine(LoginInformation _login)
+        {
+            RowDefinition rd = new RowDefinition();
+            rd.Height = new GridLength(50);
+            passwordgrid.RowDefinitions.Add(rd);
+
+            Label newWebsite = CreateLabel(_login.GetWebsite(), 1, currRowPos);
+            Label newUser = CreateLabel(_login.GetUsername(), 2, currRowPos);
+            Label newPass = CreateLabel(_login.GetPassword(), 3, currRowPos);
+
+            passwordgrid.Children.Add(newWebsite);
+            passwordgrid.Children.Add(newUser);
+            passwordgrid.Children.Add(newPass);
+        }
+
+        private Label CreateLabel(String text, int gridPosX, int gridPosY)
+        {
+            Label label = new Label();
+            label.Content = text;
+            label.Foreground = new SolidColorBrush(Colors.White);
+            label.FontSize = 25;
+            label.VerticalAlignment = VerticalAlignment.Center;
+            label.SetValue(Grid.RowProperty, gridPosY);
+            label.SetValue(Grid.ColumnProperty, gridPosX);
+            return label;
         }
     }
 }
